c72af147ec3868f6c7a40f8a0126fb55714103cb
rinba <q3255620496@outlook.com>
2023-05-15 15:38:29 +0800

    Layout静态资源

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>我是首页</div>
-    <!--二级路由出口组件（呈现位置）-->
-    <RouterView/>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView /> <!--二级路由出口区域-->
+  <LayoutFooter />
 </template>
