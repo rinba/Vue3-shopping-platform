5149d6ae196b5f6a816db4842c3a146406765191
rinba <q3255620496@outlook.com>
2023-05-18 14:57:04 +0800

    修改mian.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
 
+
+import router from './router'
+import { createPinia } from 'pinia' 
 //初始化样式文件
 import '@/styles/common.scss'
-
 //懒加载指令插件并且注册
 import { lazyPlugin } from '@/directives'
 
+
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
+app.use(createPinia())
 app.use(lazyPlugin)
-
 app.mount('#app')
