33b6f2ddaab81be60720487a600ae871f4539d94
rinba <q3255620496@outlook.com>
2023-05-18 12:23:44 +0800

    home-修改

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,4 +1,4 @@
-//封装接口函数
+//封装3个接口函数
 
 import httpInstance from '@/utils/http'
 
@@ -6,5 +6,19 @@ import httpInstance from '@/utils/http'
 export function getBannerAPI(){
     return httpInstance({
         url:'/home/banner'
+    })
+}
+
+//获取新鲜好物
+export const findNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+}
+
+//获取人气推荐
+export const findHotAPI = ()=>{
+    return httpInstance({
+        url:'/home/hot'
     })
 }
