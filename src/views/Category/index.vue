315897d30bde004cbc2dcdfd985c4ef525718c25
rinba <q3255620496@outlook.com>
2023-05-19 16:09:23 +0800

    category-使用逻辑函数拆分业务

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,40 +1,12 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/category';
-import { onMounted,ref } from 'vue'
-import { useRoute } from 'vue-router' //在组件中获取路由参数
-//组件级路由守卫。在当前路由改变，但是该组件被复用时调用
-import { onBeforeRouteUpdate} from 'vue-router' 
-import { getBannerAPI } from '@/apis/home';
 import GoodsItem from '../Home/components/GoodsItem.vue';
+import {useBanner} from './composables/useBanner' 
+import {useCategory} from './composables/useCategory' 
 
-//根据路由参数的id得到响应式接口数据
-const categoryData = ref({}) //注意这里定义的是一个对象，因为接口中的result是一个object
-const route = useRoute()
-const getCategory = async (id = route.params.id)=>{
-    const res = await getCategoryAPI(id) //此处是封装接口函数时params中的id
-    categoryData.value = res.result
-}
-onMounted(()=>{
-    getCategory()
-})
-onBeforeRouteUpdate((to)=>{
-    //当路由参数的id变化时，得到最新的响应式接口数据
-    getCategory(to.params.id)
-})
-
-//获取banner
-const bannerList = ref([])
-const getBanner = async ()=>{
-  const res = await getBannerAPI({
-    distributionSite:2 //将请求参数distributionSite改为2
-  })
-  //测试是否得到接口中的数据
-  //console.log(res) 
-  bannerList.value = res.result
-}
-onMounted(() => {
-  getBanner()
-})
+//获取bannerList，解构赋值
+const {bannerList} = useBanner()
+//获取categoryData
+const {categoryData} = useCategory()
 </script>
 
 <template>
