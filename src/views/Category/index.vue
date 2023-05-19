8f1388a1c3a0255b97c50ce914e48c72d6668007
rinba <q3255620496@outlook.com>
2023-05-19 14:43:10 +0800

    category-激活状态显示和分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -5,6 +5,7 @@ import { useRoute } from 'vue-router' //在组件中获取路由参数
 //组件级路由守卫。在当前路由改变，但是该组件被复用时调用
 import { onBeforeRouteUpdate} from 'vue-router' 
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
 //根据路由参数的id得到响应式接口数据
 const categoryData = ref({}) //注意这里定义的是一个对象，因为接口中的result是一个object
@@ -55,6 +56,26 @@ onMounted(() => {
             alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!--全部分类-->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
