f1f444e46facdfe8899c38b047cdabc866e757c9
rinba <q3255620496@outlook.com>
2023-05-19 21:19:05 +0800

    二级分类-基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,30 +1,61 @@
 <script setup>
+import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category';
+import { onMounted,ref } from 'vue'
+import { useRoute } from 'vue-router' //在组件中获取路由参数
+import GoodsItem from '../Home/components/GoodsItem.vue';
+
+//获取面包屑导航数据
+const categoryData = ref({}) //接口中的object是一个对象
+const route = useRoute()
+const getCategoryData = async ()=>{
+    const res =await getCategoryFilterAPI(route.params.id)
+    categoryData.value = res.result
+}
+onMounted(()=> getCategoryData())
+
+//获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: 1005000 ,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+const getGoodList = async ()=>{
+    const res = await getSubCategoryAPI(reqData.value)
+    console.log(res)
+    goodList.value = res.result.items
+}
+onMounted(()=>{
+    getGoodList()
+})
 
 
 </script>
 
 <template>
-  <div class="container ">
-    <!-- 面包屑 -->
-    <div class="bread-container">
-      <el-breadcrumb separator=">">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
-      </el-breadcrumb>
+    <div class="container ">
+        <!-- 面包屑 -->
+        <div class="bread-container">
+            <el-breadcrumb separator=">">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }"> {{ categoryData.parentName }}
+            </el-breadcrumb-item>
+            <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="sub-container">
+            <el-tabs>
+                <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
+                <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
+                <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
+            </el-tabs>
+            <div class="body">
+                <!-- 商品列表-->
+                <GoodsItem v-for="goods in goodList" :goods="goods" key="goods.id"/>
+            </div>
+        </div>
     </div>
-    <div class="sub-container">
-      <el-tabs>
-        <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
-        <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
-        <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
-      </el-tabs>
-      <div class="body">
-         <!-- 商品列表-->
-      </div>
-    </div>
-  </div>
 
 </template>
 
