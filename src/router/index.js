9b0d87975ec98fc882b920698be6970f714d49d6
rinba <q3255620496@outlook.com>
2023-05-19 18:19:59 +0800

    二级分类-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,10 @@ const router = createRouter({
           path:'category/:id',   //写子路由时路径前面没有/ 
           component:Category
 
+        },
+        {
+          path:'category/sub/:id',
+          component:SubCategory
         }
       ]
     },
