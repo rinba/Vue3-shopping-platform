eefc3677dd8a04e84c986059129e058e0199e5dd
rinba <q3255620496@outlook.com>
2023-05-20 11:39:32 +0800

    二级路由-商品列表实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -14,8 +14,8 @@ const getCategoryData = async ()=>{
 onMounted(()=> getCategoryData())
 
 //获取基础列表数据渲染
-const goodList = ref([])
-const reqData = ref({
+const goodList = ref([]) //商品列表
+const reqData = ref({  //封装接口函数时的基础参数data（对象）
     categoryId: 1005000 ,
     page: 1,
     pageSize: 20,
@@ -29,6 +29,29 @@ const getGoodList = async ()=>{
 onMounted(()=>{
     getGoodList()
 })
+
+//tab切换回调
+const tabChange = ()=>{
+  //console.log('tab切换了',reqData.value.sortField)
+  reqData.value.page = 1 //限制页数
+  getGoodList()
+}
+
+//加载更多
+const disabled =ref(false)
+const load = async ()=>{
+  //console.log('加载更多数据了')
+  //获取下一页的数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  //使用ES6的扩展运算符拼接老数据、新数据
+  goodList.value = [...goodList.value , ...res.result.items]
+  
+  //加载完毕，停止监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -43,12 +66,12 @@ onMounted(()=>{
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodsItem v-for="goods in goodList" :goods="goods" key="goods.id"/>
             </div>
