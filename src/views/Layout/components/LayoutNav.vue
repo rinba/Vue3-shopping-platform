a9c9bd78c9e6804f98d51a07ad7b45c06dbe22b1
rinba <q3255620496@outlook.com>
2023-05-22 22:56:17 +0800

    登录页-退出登录

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -2,6 +2,17 @@
 //Pinia
 import { useUserStore } from '@/stores/user';
 const userStore = useUserStore()
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const confirm = ()=>{
+  //console.log('用户要退出登录了')
+  //退出登录时业务逻辑实现
+  //1.清除当前用户信息（触发action）
+  userStore.clearUserInfo()
+  //2.跳转到登录页
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -14,7 +25,7 @@ const userStore = useUserStore()
             <template v-if="userStore.userInfo.token">
             <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
             <li>
-                <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+                <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
                 <template #reference>
                     <a href="javascript:;">退出登录</a>
                 </template>
