8b85832fbf82beeaab2b9fb6bce624634a4bdc6b
rinba <q3255620496@outlook.com>
2023-05-22 21:12:33 +0800

    登录页-Pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -10,10 +10,14 @@ import '@/styles/common.scss'
 import { lazyPlugin } from '@/directives'
 //引入全局组件插件并且注册
 import { componentPlugin } from './components'
+//引入Pinia持久化插件
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 app.use(router)
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
+pinia.use(piniaPluginPersistedstate) //注册Pinia持久化插件
 app.use(lazyPlugin)
 app.use(componentPlugin)
 app.mount('#app')
