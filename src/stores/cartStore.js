56ce4769b781d303eb0c702db1728e9273097af3
rinba <q3255620496@outlook.com>
2023-05-23 17:20:55 +0800

    购物车-本地购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -7,9 +7,9 @@ export const useCartStore = defineStore('cart',()=>{
     //1.定义state
     const cartList = ref([])    
     //2.定义action
+    //2.1.添加购物车
     //此处的goods是从Detail->index.vue中传过来的参数，和接口中goods不一样
     const addCart = (goods)=>{
-        //添加购物车操作
         //已添加过，count+1；没有添加过，直接push
         //通过匹配传递过来的商品对象中的skuId能不能在cartlist中找到，找到了就是添加过
         const item = cartList.value.find((item)=>goods.skuId === item.skuId)
@@ -22,9 +22,19 @@ export const useCartStore = defineStore('cart',()=>{
         }
     
     }   
+
+    //2.2.删除购物车（依靠skuId）
+    const delCart = (skuId)=>{
+        //实现数组的定点删除有两种办法：splice、filter
+        //找到要删除项的下标值，使用splice（替换，会改变原数组）
+        const idx = cartList.value.findIndex((item)=> skuId === item.skuId)
+        cartList.value.splice(idx,1)
+    }
+    
     return{
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 },{
     persist:true
--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -2,6 +2,7 @@
 import { useCartStore } from '@/stores/cartStore'
 const cartStore = useCartStore()
 
+
 </script>
 
 <template>
@@ -25,7 +26,7 @@ const cartStore = useCartStore()
               <p class="count">x{{ i.count }}</p>
             </div>
           </RouterLink>
-          <i class="iconfont icon-close-new" @click="store.delCart(i.skuId)"></i>
+          <i class="iconfont icon-close-new" @click="cartStore.delCart(i.skuId)"></i>
         </div>
       </div>
       <div class="foot">
