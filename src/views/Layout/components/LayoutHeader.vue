e82ee3cac4d9e71a53571bce19178c59634683ce
rinba <q3255620496@outlook.com>
2023-05-23 15:55:17 +0800

    购物车-头部购物车渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category'
 const categoryStore = useCategoryStore()
+import HeaderCart from './HeaderCart.vue'
 </script>
 
 <template>
@@ -22,6 +23,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
+      <HeaderCart/>
     </div>
   </header>
 </template>
