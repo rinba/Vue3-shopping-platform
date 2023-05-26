6fe4fb59dfe4f9fa694a31dc6265c88faa7111da
rinba <q3255620496@outlook.com>
2023-05-26 12:16:39 +0800

    会员中心-细节优化

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -43,6 +43,18 @@ const pageChange = (page)=>{
   getOrderList() //重新发送请求，获取当前页数下的订单列表
 }
 
+// 创建格式化函数
+const fomartPayState = (payState) => {
+  const stateMap = {
+    1: '待付款',
+    2: '待发货',
+    3: '待收货',
+    4: '待评价',
+    5: '已完成',
+    6: '已取消'
+  }
+  return stateMap[payState]
+}
 </script>
 
 <template>
@@ -88,7 +100,7 @@ const pageChange = (page)=>{
                 </ul>
               </div>
               <div class="column state">
-                <p>{{ order.orderState }}</p>
+                <p>{{ fomartPayState(order.orderState) }}</p>
                 <p v-if="order.orderState === 3">
                   <a href="javascript:;" class="green">查看物流</a>
                 </p>
